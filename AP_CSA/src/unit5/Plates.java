b17edb553fbbd34b214b1f9f376293e1b2ec6aae
renj3065 <renj3065@CA-SU-F106-34.sduhsd.lan>
2018-02-14 14:17:59 -0800

    Test delete

--- AP_CSA/src/unit5/Plates.java
+++ AP_CSA/src/unit5/Plates.java
@@ -1,11 +1,13 @@
 package unit5;
 
+import java.util.Random;
+
 public class Plates {\
 	private String blank;
 public Plates(){
 	blank="";
 }
-public String generate(){
-	
+public void generate(){
+   blank+=Random.ints(65,91);
 }
 }
--- AP_CSA/src/unit5/TestRepositoryClass.java
+++
@@ -1,5 +0,0 @@
-package unit5;
-
-public class TestRepositoryClass {
-
-}
