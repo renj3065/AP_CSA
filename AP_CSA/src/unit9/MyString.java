b98db063f28394111bbe4e7a7df4674479f74d2d
renj3065 <renj3065@CA-SU-F106-34.sduhsd.lan>
2018-02-26 15:00:20 -0800

    Elevens

---
+++ AP_CSA/src/unit9/MyString.java
@@ -0,0 +1,52 @@
+package unit9;
+
+public class MyString {
+	char[] strng;
+	public MyString(char[] chars){
+		strng=chars;
+		
+	}
+	public char charAt(int index){
+		return strng[index];
+	}
+	public int length(){
+		int count=0;
+		
+		for(char x: strng){
+			count++;
+		}
+		
+		return count;
+	}
+	public MyString substring(int begin, int end){
+		char[] temp=new char[end-begin];
+		for(int x=begin;x<end;x++){
+			temp[x-begin]=strng[x];
+		}
+		MyString sub=new MyString(temp);
+		
+		return sub;
+
+		
+	}
+	public MyString toLowerCase(){
+		char[] temp=strng;
+		int index=0;
+		for(char x:strng)
+		{
+			if (x>64 && x<91)
+				temp[index]=(char)((int)temp[index]+32);
+			index++;
+		}
+		
+		MyString low=new MyString(temp);
+		return low;
+	}
+	public boolean equals(MyString s){
+		
+	}
+	public static MyString valueOf(int i);
+				
+				
+
+}
