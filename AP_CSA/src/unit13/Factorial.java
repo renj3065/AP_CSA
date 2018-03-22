2feae4124cbdb558a6546930cea4c8384d9543c1
renj3065 <renj3065@CA-SU-F106-34.sduhsd.lan>
2018-03-22 13:52:04 -0700

    Jurnil

--- AP_CSA/src/unit13/Factorial.java
+++ AP_CSA/src/unit13/Factorial.java
@@ -1,5 +1,7 @@
 package unit13;
 
+import java.util.Random;
+
 public class Factorial {
 public Factorial(int x){
 	calc(x);
@@ -35,12 +37,71 @@ public int FindMax(int[] x, int index){
 	
 	
 }
+public void generateMatrix(){
+	int[][] temp=new int[6][6];
+	Random r=new Random();
+	for(int x=0;x<temp.length;x++){
+		for(int y=0;y<temp[0].length;y++){
+			temp[x][y]=r.nextInt(2);
+		}
+	}
+	String output="";
 
+
+	for (int i = 0; i < temp.length; i++) {
+	    for (int j = 0; j < temp[i].length; j++) {
+	        output+=temp[i][j] + " ";
+	    }
+	    	output+="\n";
+	}
+	
+	System.out.println(output);
+	boolean even=true;
+	int sum;
+	for(int x=0;x<6;x++){
+		even=true;
+		sum=0;
+		for(int y=0;y<6;y++){
+			sum+=temp[x][y];
+		}
+		if(sum%2==1){
+			even=false;
+		}
+		if(even){
+			System.out.println("There is an even number of 1s in row "+x);
+		}
+		else
+			System.out.println("There is an odd number of 1s in row "+x);
+	}
+	
+
+	
+	 even=true;
+	for(int x=0;x<6;x++){
+		even=true;
+		sum=0;
+		for(int y=0;y<6;y++){
+			sum+=temp[y][x];
+		}
+		if(sum%2==1){
+			even=false;
+		}
+		if(even){
+			System.out.println("There is an even number of 1s in column "+x);
+		}
+		else
+			System.out.println("There is an odd number of 1s in column "+x);
+	}
+	
+	
+}
 public static void main(String[] args){
 	Factorial test=new Factorial(10);
 	System.out.println(test.calc(10));
 	System.out.println(test.recCalc(10,10));
 	int[] temp={1,2,3};
 System.out.println(test.FindMax(temp, temp.length-1));
+test.generateMatrix();
+
 }
 }
