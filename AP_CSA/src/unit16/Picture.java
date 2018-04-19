50c11a3d8d31a1b841260a83cd78fbdfd8a42284
jren7 <jren7@DESKTOP-J3KQDA3.san.rr.com>
2018-04-18 22:35:44 -0700

    wa

--- AP_CSA/src/unit16/Picture.java
+++ AP_CSA/src/unit16/Picture.java
@@ -407,7 +407,53 @@ if(!(pixelObj.getBlue()>150 && pixelObj.getRed()<30)){
           leftPixel.setColor(Color.WHITE);
       }
     }
+    
+    
   }
+
+  public void edgeDetection2(int edgeDist)
+  {
+    Pixel leftPixel = null;
+    Pixel rightPixel = null;
+    Pixel leftPixel2 = null;
+
+    Pixel topPixel = null;
+    Pixel bottomPixel = null;
+    Pixel[][] pixels = this.getPixels2D();
+
+    Color rightColor = null;
+    Color bottomColor = null;
+
+    for (int row = 0; row < pixels.length-1; row++)
+    {
+      for (int col = 0; 
+           col < pixels[0].length-1; col++)
+      {
+        leftPixel = pixels[row][col];
+        leftPixel2= pixels[row][col+1];
+
+        rightPixel = pixels[row][col+1];
+        rightColor = rightPixel.getColor();
+  
+        bottomPixel = pixels[row+1][col];
+        bottomColor=bottomPixel.getColor();
+        if (leftPixel.colorDistance(rightColor) > 
+            edgeDist)
+          leftPixel.setColor(Color.BLACK);
+        else
+          leftPixel.setColor(Color.WHITE);
+        
+        
+        if (leftPixel2.colorDistance(bottomColor) >
+        edgeDist)
+        leftPixel.setColor(Color.BLACK);
+        else
+        leftPixel.setColor(Color.WHITE);
+        }
+      }
+    }
+
+
   
   public void myCollage(){
 	 
